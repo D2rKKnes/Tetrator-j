4a89e4ba6d13bf593ec4bce0b8b972048ab7746a
The_al0ne <strashno56@gmail.com>
2026-04-21 14:25:18 +0300

    Update TerraMod.java

---
+++ src/example/TerraMod.java
@@ -0,0 +1,12 @@
+package terra;
+
+import arc.*;
+import arc.util.*;
+import mindustry.game.EventType.*;
+import mindustry.mod.*;
+import mindustry.ui.dialogs.*;
+import terra.content.TerraItems
+
+public class TerraMod extends Mod{
+//Nothing to see now!
+}
