5fcf9fc7d5f0d51f900f73dc4e8739ee5e3d7d56
The_al0ne <strashno56@gmail.com>
2026-04-21 14:53:51 +0300

    fix

--- src/terra/TerraMod.java
+++ src/terra/TerraMod.java
@@ -5,7 +5,7 @@ import arc.util.*;
 import mindustry.game.EventType.*;
 import mindustry.mod.*;
 import mindustry.ui.dialogs.*;
-import terra.content.TerraItems
+import terra.content.TerraItems;
 
 public class TerraMod extends Mod{
 //Nothing to see now!
