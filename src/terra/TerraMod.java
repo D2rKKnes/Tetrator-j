5f48ad7bf2f266a77a846b752d1b41b42eff5b34
The_al0ne <strashno56@gmail.com>
2026-04-21 16:12:43 +0300

    fixxxx

--- src/terra/TerraMod.java
+++ src/terra/TerraMod.java
@@ -5,7 +5,7 @@ import arc.util.*;
 import mindustry.game.EventType.*;
 import mindustry.mod.*;
 import mindustry.ui.dialogs.*;
-import terra.content.TerraItems;
+import terra.content.*;
 
 public class TerraMod extends Mod{
     @Override
