45e8b69db8e2c559d220b0fd937dfb9a12d33cca
The_al0ne <strashno56@gmail.com>
2026-04-22 12:54:03 +0300

    Create first unit

---
+++ src/terra/content/TerraUnitTypes.java
@@ -0,0 +1,79 @@
+package terra.content;
+
+import arc.graphics.*;
+import arc.graphics.g2d.*;
+import arc.math.*;
+import arc.math.geom.*;
+import arc.struct.*;
+import arc.util.*;
+import mindustry.ai.*;
+import mindustry.ai.types.*;
+import mindustry.annotations.Annotations.*;
+import mindustry.entities.*;
+import mindustry.entities.abilities.*;
+import mindustry.entities.bullet.*;
+import mindustry.entities.effect.*;
+import mindustry.entities.part.*;
+import mindustry.entities.pattern.*;
+import mindustry.gen.*;
+import mindustry.graphics.*;
+import mindustry.type.*;
+import mindustry.type.ammo.*;
+import mindustry.type.unit.*;
+import mindustry.type.weapons.*;
+import mindustry.world.meta.*;
+
+import static arc.graphics.g2d.Draw.*;
+import static arc.graphics.g2d.Lines.*;
+import static arc.math.Angles.*;
+import static mindustry.Vars.*;
+
+public class TerraUnitTypes {
+    public static UnitType
+    
+    wick;
+
+    public static void load() {
+        wick = new UnitType("wick"){{
+            flying = true;
+            speed = 3.1f;
+            drag = 0.04f;
+            accel = 0.08f;
+            hitSize = 8f;
+            health = 95;
+            engineSize = 2.25f;
+            engineOffset = 5.25f;
+            range = 40f;
+            itemCapacity = 10;
+            ammoType = new ItemAmmoType(TerraItems.carbon);
+            createWreck = false;
+
+            weapons.add(new Weapon(){{
+                shootOnDeath = true;
+                targetUnderBlocks = false;
+                reload = 24f;
+                shootCone = 180f;
+                ejectEffect = Fx.none;
+                shootSound = Sounds.explosionCrawler;
+                shootSoundVolume = 0.4f;
+                x = shootY = 0f;
+                mirror = false;
+                bullet = new BulletType(){{
+                    collidesTiles = false;
+                    collides = false;
+
+                    rangeOverride = 25f;
+                    hitEffect = Fx.pulverize;
+                    speed = 0f;
+                    splashDamageRadius = 44f;
+                    instantDisappear = true;
+                    splashDamage = 70f;
+                    buildingDamageMultiplier = 0.68f;
+                    killShooter = true;
+                    hittable = false;
+                    collidesAir = true;
+                }};
+            }});
+        }};
+    }
+}
