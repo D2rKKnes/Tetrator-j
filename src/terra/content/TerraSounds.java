5f0fb99284d09a69953c066c28642bd3dcae5463
The_al0ne <strashno56@gmail.com>
2026-04-22 21:53:41 +0300

    Create TerraSounds.java

---
+++ src/terra/content/TerraSounds.java
@@ -0,0 +1,35 @@
+package terra.content;
+
+import arc.Core;
+import arc.assets.AssetDescriptor;
+import arc.assets.loaders.SoundLoader;
+import arc.audio.Sound;
+import arc.struct.Seq;
+import mindustry.Vars;
+
+import java.lang.reflect.Field;
+
+public class TerraSounds {
+    public static Sound
+            shootLaunch = new Sound(), railGunCharge = new Sound();
+    public static void load(){
+                shootLaunch = loadSound("shootLaunch"), railGunCharge = loadSound("railGunCharge");
+    }
+
+    private static Sound loadSound(String soundName){
+        if(!Vars.headless){
+            String name = "sounds/" + soundName;
+            String path = Vars.tree.get(name + ".ogg").exists() ? name + ".ogg" : name + ".mp3";
+
+            Sound sound = new Sound();
+
+            AssetDescriptor<?> desc = Core.assets.load(path, Sound.class, new SoundLoader.SoundParameter(sound));
+            desc.errored = Throwable::printStackTrace;
+
+            return sound;
+
+        }else{
+            return new Sound();
+        }
+    }
+}
