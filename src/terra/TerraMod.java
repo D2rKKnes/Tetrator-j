309c85b159620cd559ad6f675298eee88e430eea
The_al0ne <strashno56@gmail.com>
2026-04-24 12:39:26 +0300

    Fix laser

--- src/terra/TerraMod.java
+++ src/terra/TerraMod.java
@@ -6,10 +6,12 @@ import mindustry.game.EventType.*;
 import mindustry.mod.*;
 import mindustry.ui.dialogs.*;
 import terra.content.*;
+import terra.util.*;
 
 public class TerraMod extends Mod{
     @Override
     public void loadContent(){
+        Utils.init();
         TerraItems.load();
         TerraSounds.load();
         TerraEnvironmentBlocks.load();
