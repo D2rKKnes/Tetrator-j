8facf08088007ea54d92a16c238d974ff6d609d5
The_al0ne <strashno56@gmail.com>
2026-04-25 16:09:09 +0300

    Update TerraBlocks.java

--- src/terra/content/TerraBlocks.java
+++ src/terra/content/TerraBlocks.java
@@ -46,7 +46,7 @@ import static arc.graphics.g2d.Lines.*;
 public class TerraBlocks{
     public static Block
     //walls
-    scrapWallColossol, metaglassWall;
+    scrapWallColossol, metaglassWall, metaglassWallLarge, metaglassWallHuge, darkSteelWall, darkSteelWallLarge;
     //distrubution
 
     //power
@@ -84,7 +84,75 @@ public class TerraBlocks{
             }};
             hitBulletAmount = 9;
             hitBulletAmountRand = 3;
-            hitBulletSpawnChance = 0.08f;
+            hitBulletSpawnChance = 0.12f;
+            hitBulletEffect = new WaveEffect(){{
+                sizeFrom = strokeTo = 0f;
+                sizeTo = 20f;
+                strokeFrom = 2f;
+                lifetime = 20f;
+                colorFrom = Pal.glassAmmoFront;
+                colorTo = Pal.glassAmmoBack;
+            }};
+            hitBulletSound = Sounds.explosionDull;
+            hitBulletSoundPitchMin = 3.8f;
+            hitBulletSoundPitchMax = 4f;
+            hitBulletLifeRandScl = 0.3f;
+            hitBulletSpeedRandScl = 0.4f;
+        }};
+        metaglassWallLarge = new AdvancedWall("metaglass-wall-large"){{
+            requirements(Category.defense, with(Items.metaglass, 6 * 4));
+            health = 380 * 4;
+            size = 2;
+            hitBullet = new BasicBulletType(){{
+                damage = 21f;
+                lifetime = 30f;
+                width = 12f;
+                height = 26f;
+                shrinkY = 0.4f;
+                shrinkX = 1f;
+                speed = 2f;
+                backColor = hitColor = trailColor = Pal.glassAmmoBack;
+                frontColor = Pal.glassAmmoFront;
+                despawnEffect = Fx.none;
+                ammoMultiplier = 1f;
+            }};
+            hitBulletAmount = 11;
+            hitBulletAmountRand = 3;
+            hitBulletSpawnChance = 0.12f;
+            hitBulletEffect = new WaveEffect(){{
+                sizeFrom = strokeTo = 0f;
+                sizeTo = 30f;
+                strokeFrom = 2.5f;
+                lifetime = 20f;
+                colorFrom = Pal.glassAmmoFront;
+                colorTo = Pal.glassAmmoBack;
+            }};
+            hitBulletSound = Sounds.explosionDull;
+            hitBulletSoundPitchMin = 3.8f;
+            hitBulletSoundPitchMax = 4f;
+            hitBulletLifeRandScl = 0.3f;
+            hitBulletSpeedRandScl = 0.4f;
+        }};
+        metaglassWallHuge = new AdvancedWall("metaglass-wall-huge"){{
+            requirements(Category.defense, with(Items.metaglass, 6 * 9));
+            health = 380 * 9;
+            size = 3;
+            hitBullet = new BasicBulletType(){{
+                damage = 21f;
+                lifetime = 30f;
+                width = 12f;
+                height = 26f;
+                shrinkY = 0.4f;
+                shrinkX = 1f;
+                speed = 2f;
+                backColor = hitColor = trailColor = Pal.glassAmmoBack;
+                frontColor = Pal.glassAmmoFront;
+                despawnEffect = Fx.none;
+                ammoMultiplier = 1f;
+            }};
+            hitBulletAmount = 13;
+            hitBulletAmountRand = 3;
+            hitBulletSpawnChance = 0.12f;
             hitBulletEffect = new WaveEffect(){{
                 sizeFrom = strokeTo = 0f;
                 sizeTo = 40f;
@@ -96,6 +164,26 @@ public class TerraBlocks{
             hitBulletSound = Sounds.explosionDull;
             hitBulletSoundPitchMin = 3.8f;
             hitBulletSoundPitchMax = 4f;
+            hitBulletLifeRandScl = 0.3f;
+            hitBulletSpeedRandScl = 0.4f;
+        }};
+        darkSteelWall = new AdvancedWall("dark-steel-wall"){{
+            requirements(Category.defense, with(TerraItems.darkSteel, 6 * 4, Items.metaglass, (6 * 4) / 2));
+            health = 930 * 4;
+            size = 2;
+            absorbLasers = true;
+            autoRegeneration = true;
+            regenStartDelay = 150f;
+            regenDamageStop = true;
+        }};
+        darkSteelWallLarge = new AdvancedWall("dark-steel-wall-large"){{
+            requirements(Category.defense, with(TerraItems.darkSteel, 6 * 9, Items.metaglass, (6 * 9) / 2));
+            health = 930 * 9;
+            size = 3;
+            absorbLasers = true;
+            autoRegeneration = true;
+            regenStartDelay = 150f;
+            regenDamageStop = true;
         }};
     }
 }
