f217a226cdb2eada59228bf22ce5ba96bba1b475
The_al0ne <strashno56@gmail.com>
2026-04-25 13:00:37 +0300

    Create TerraBlocks.java
    begins...

---
+++ src/terra/content/TerraBlocks.java
@@ -0,0 +1,55 @@
+package terra.content;
+
+import terra.type.bullet.*;
+import arc.util.Tmp;
+import mindustry.entities.part.*;
+import mindustry.entities.pattern.*;
+import arc.graphics.*;
+import arc.graphics.g2d.*;
+import arc.math.*;
+import arc.math.geom.*;
+import mindustry.entities.effect.*;
+import mindustry.content.*;
+import mindustry.entities.*;
+import mindustry.entities.bullet.*;
+import mindustry.type.*;
+import mindustry.type.unit.*;
+import mindustry.entities.effect.MultiEffect;
+import mindustry.gen.*;
+import mindustry.graphics.*;
+import mindustry.world.*;
+import mindustry.world.blocks.defense.turrets.*;
+import mindustry.world.blocks.units.*;
+import mindustry.world.consumers.ConsumeLiquid;
+import mindustry.world.draw.*;
+import mindustry.world.meta.BuildVisibility;
+import mindustry.world.meta.Env;
+
+import static mindustry.type.ItemStack.*;
+import static arc.graphics.g2d.Draw.*;
+import static arc.graphics.g2d.Lines.*;
+
+public class TerraBlocks{
+    public static Block
+    //walls
+    scrapWallColossol;
+    //distrubution
+
+    //power
+
+    //crafters
+
+    //turrets
+
+    //units
+
+    //other
+    public static void load(){
+        scrapWallColossol = new Wall("scrap-wall-colossol"){{
+            requirements(Category.defense, ItemStack.mult(scrapWall.requirements, 25));
+            health = 60 * 25 * wallHealthMultiplier;
+            size = 5;
+            buildCostMultiplier = 4f;
+        }};
+    }
+}
