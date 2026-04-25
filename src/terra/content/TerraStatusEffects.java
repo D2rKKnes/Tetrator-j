4aae5a2abeb6fc849fc207e85fe90428f514bf86
The_al0ne <strashno56@gmail.com>
2026-04-25 19:43:08 +0300

    Update TerraStatusEffects.java

--- src/terra/content/TerraStatusEffects.java
+++ src/terra/content/TerraStatusEffects.java
@@ -1,12 +1,26 @@
 package terra.content;
 
-import arc.Events;
-import arc.graphics.*;
-import mindustry.content.Fx;
-import mindustry.content.StatusEffects;
-import mindustry.game.EventType;
-import mindustry.type.*;
-import mindustry.entities.effect.*;
+import arc.graphics.Blending;
+import arc.graphics.Color;
+import arc.graphics.g2d.Draw;
+import arc.graphics.g2d.Fill;
+import arc.graphics.g2d.Lines;
+import arc.math.Interp;
+import arc.math.Mathf;
+import arc.math.Rand;
+import arc.math.geom.Geometry;
+import arc.struct.Seq;
+import arc.util.Time;
+import arc.util.Tmp;
+import mindustry.Vars;
+import mindustry.entities.Effect;
+import mindustry.entities.effect.MultiEffect;
+import mindustry.entities.units.StatusEntry;
+import mindustry.gen.Unit;
+import mindustry.graphics.Drawf;
+import mindustry.graphics.Layer;
+import mindustry.graphics.Pal;
+import mindustry.type.StatusEffect;
 
 import static mindustry.Vars.*;
 import static mindustry.content.StatusEffects.sapped;
