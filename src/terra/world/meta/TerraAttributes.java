322443f81bf650e10b57fbad019bdef840221304
The_al0ne <strashno56@gmail.com>
2026-04-28 21:22:37 +0300

    Create TerraAttributes.java

---
+++ src/terra/world/meta/TerraAttributes.java
@@ -0,0 +1,8 @@
+package terra.world.meta;
+
+import mindustry.world.meta.*;
+
+public class TerraAttributes {
+    public static final Attribute carbon = Attribute.add("carbon");
+    public static final Attribute ice = Attribute.add("ice");
+}
