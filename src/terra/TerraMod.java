c416efc73e1e18c69b0f573af46780d1877e23a8
The_al0ne <strashno56@gmail.com>
2026-05-05 20:31:49 +0300

    Update TerraMod.java

--- src/terra/TerraMod.java
+++ src/terra/TerraMod.java
@@ -2,13 +2,22 @@ package terra;
 
 import arc.*;
 import arc.util.*;
+import arc.Events;
 import mindustry.game.EventType.*;
 import mindustry.mod.*;
 import mindustry.ui.dialogs.*;
+import mindustry.content.Planets;
 import terra.content.*;
 import terra.util.*;
 
 public class TerraMod extends Mod{
+    public MyMod(){
+        Events.on(ClientLoadEvent.class, e -> {
+            Planets.verilus.generator = new VerilusAsteroidGenerator();
+            Planets.verilus.rebuild();
+        });
+    }
+    
     @Override
     public void loadContent(){
         Utils.init();
