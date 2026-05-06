14d53634c6e6ba433ae9d5e6fa01c5aa47b0db01
The_al0ne <strashno56@gmail.com>
2026-05-07 00:03:13 +0300

    Update DrawDrillOreTop.java

--- src/terra/world/drawer/DrawDrillOreTop.java
+++ src/terra/world/drawer/DrawDrillOreTop.java
@@ -8,7 +8,7 @@ import mindustry.world.draw.DrawBlock;
 public class DrawDrillOreTop extends DrawBlock {
     @Override
     public void draw(Building build) {
-        if (build instanceof Drill.DrillBuild drill && drill.dominantItem != null && block.itemRegion != null && block.itemRegion.found()) {
+        if (build instanceof Drill.DrillBuild drill && drill.dominantItem != null && drill.itemRegion != null && drill.itemRegion.found()) {
             Draw.color(drill.dominantItem.color);
             Draw.rect(((Drill)drill.block).itemRegion, drill.x, drill.y);
             Draw.color();
