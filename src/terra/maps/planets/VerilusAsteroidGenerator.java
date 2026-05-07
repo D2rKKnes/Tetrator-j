a196d12a8ff268398f9cb497ba3d53cb21b2c8b4
The_al0ne <strashno56@gmail.com>
2026-05-07 13:49:09 +0300

    fff

--- src/terra/maps/planets/VerilusAsteroidGenerator.java
+++ src/terra/maps/planets/VerilusAsteroidGenerator.java
@@ -72,7 +72,7 @@ public class VerilusAsteroidGenerator extends BlankPlanetGenerator{
         tiles.eachTile(t -> t.setFloor(background));
 
         //the center asteroid is always stone
-        asteroid(sx, sy, rand.random(38f, radMax), Blocks.stone.asFloor());
+        asteroid(sx, sy, rand.random(34, 68), Blocks.stone.asFloor());
 
         float radr = 170f + Mathf.random(10f, 50f);
         float anglr = Mathf.random(360f);
@@ -80,7 +80,7 @@ public class VerilusAsteroidGenerator extends BlankPlanetGenerator{
         int yr = sy + (int)(Mathf.sinDeg(anglr) * radr);
 
         //special asteroid for core zone also is always stone
-        asteroid(xr, yr, rand.random(38f, radMax), Blocks.stone.asFloor());
+        asteroid(xr, yr, rand.random(34, 68), Blocks.stone.asFloor());
 
         //spawn asteroids
         int amount = rand.random(min, max);
