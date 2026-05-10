2ef1fa8d14ee0d4b08ac3216f7a21cc711048639
The_al0ne <strashno56@gmail.com>
2026-05-10 10:45:48 +0300

    Update TerraStatusEffects.java

--- src/terra/content/TerraStatusEffects.java
+++ src/terra/content/TerraStatusEffects.java
@@ -1,7 +1,7 @@
 package terra.content;
 
-import arc.graphics.Blending;
-import arc.graphics.Color;
+import arc.*;
+import arc.graphics.*;
 import arc.graphics.g2d.*;
 import arc.math.*;
 import arc.math.geom.Geometry;
@@ -28,8 +28,11 @@ import mindustry.entities.bullet.*;
 import mindustry.type.*;
 import mindustry.type.unit.*;
 
+import static arc.graphics.g2d.Draw.rect;
+import static arc.graphics.g2d.Draw.*;
+import static arc.graphics.g2d.Lines.*;
+import static arc.math.Angles.*;
 import static mindustry.Vars.*;
-import static mindustry.content.StatusEffects.sapped;
 
 public class TerraStatusEffects{
     public static StatusEffect 
