5712da36c4d56a58b40a6662ebb8a7c415ada737
The_al0ne <strashno56@gmail.com>
2026-05-20 21:41:44 +0300

    Update DrawVariantRegion.java

--- src/terra/world/drawer/DrawVariantRegion.java
+++ src/terra/world/drawer/DrawVariantRegion.java
@@ -10,7 +10,7 @@ import mindustry.world.draw.DrawBlock;
 public class DrawVariantRegion extends DrawBlock {
     public String suffix = "-variant";
     public int variants = 0; 
-    public boolean randOffset = false;
+    public int randOffset = 0;
 
     protected TextureRegion[] variantRegions;
 
@@ -37,27 +37,24 @@ public class DrawVariantRegion extends DrawBlock {
     }
 
     @Override
-    public void draw(Building build) {
-        TextureRegion regionToDraw;
+    public void load(Block block) {
+        super.load(block);
         
-        if (variants == 0 || variantRegions == null) {
-            regionToDraw = build.block.region;
-        } else {
-            int index = Mathf.randomSeed(build.tile.pos(), 0, Math.max(0, variantRegions.length - 1));
-            regionToDraw = variantRegions[index];
-        }
-
-        if (randOffset) {
-            float xOffset = Mathf.randomSeed(build.tile.pos() + 1, -0.5f, 0.5f);
-            float yOffset = Mathf.randomSeed(build.tile.pos() + 2, -0.5f, 0.5f);
-            Draw.rect(regionToDraw, build.x + xOffset, build.y + yOffset, build.rotation - 90);
-        } else {
-            Draw.rect(regionToDraw, build.x, build.y, build.rotation - 90);
+        variantRegions = new TextureRegion[Math.max(1, variants)];
+        for (int i = 0; i < variantRegions.length; i++) {
+            variantRegions[i] = Core.atlas.find(block.name + suffix + (i + 1), block.region);
         }
     }
 
     @Override
+    public void draw(Building build) {
+        int index = Mathf.randomSeed(build.tile.pos() + randOffset, 0, Math.max(0, variantRegions.length - 1));
+        
+        Draw.rect(variantRegions[index], build.x, build.y);
+    }
+
+    @Override
     public TextureRegion[] icons(Block block) {
-        return new TextureRegion[]{variants > 0 && variantRegions != null ? variantRegions[0] : block.region};
+        return new TextureRegion[]{variantRegions != null && variantRegions.length > 0 ? variantRegions : block.region};
     }
 }
