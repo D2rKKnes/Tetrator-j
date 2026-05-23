16b7458d0ba36d2e8c2f67b749bfb86e1d735077
The_al0ne <strashno56@gmail.com>
2026-05-23 13:12:05 +0300

    Create DrawLiquidTileRotated.java

---
+++ src/terra/world/drawer/DrawLiquidTileRotated.java
@@ -0,0 +1,67 @@
+package terra.world.drawer;
+
+import mindustry.gen.*;
+import mindustry.type.*;
+import mindustry.world.*;
+import mindustry.world.blocks.liquid.*;
+
+public class DrawLiquidTileRotated extends DrawBlock {
+    public Liquid drawLiquid;
+    public float padding;
+    public float padLeft = -1, padRight = -1, padTop = -1, padBottom = -1;
+    public float alpha = 1f;
+    public float xOffset = 0, yOffset = 0;
+
+    public DrawLiquidTile(Liquid drawLiquid, float padding){
+        this.drawLiquid = drawLiquid;
+        this.padding = padding;
+    }
+
+    public DrawLiquidTile(Liquid drawLiquid, float xOffset, float yOffset){
+        this.drawLiquid = drawLiquid;
+        this.xOffset = xOffset;
+        this.yOffset = yOffset;
+    }
+
+    public DrawLiquidTile(Liquid drawLiquid){
+        this.drawLiquid = drawLiquid;
+    }
+
+    public DrawLiquidTile(){
+    }
+
+    @Override
+    public void draw(Building build) {
+        Liquid drawn = drawLiquid != null ? drawLiquid : build.liquids.current();
+        float pl = padLeft, pr = padRight, pt = padTop, pb = padBottom;
+        float realLeft = pl, realRight = pr, realTop = pt, realBottom = pb;
+        
+        switch(build.rotation) {
+            case 1:
+                realRight = pb; realTop = pr; realLeft = pt; realBottom = pl;
+                break;
+            case 2:
+                realRight = pl; realTop = pb; realLeft = pr; realBottom = pt;
+                break;
+            case 3:
+                realRight = pt; realTop = pl; realLeft = pb; realBottom = pr;
+                break;
+            default:
+                break;
+        }
+
+        LiquidBlock.drawTiledFrames(
+            build.block.size, build.x + xOffset, build.y + yOffset, 
+            realLeft, realRight, realTop, realBottom, 
+            drawn, build.liquids.get(drawn) / build.block.liquidCapacity * alpha
+        );
+    }
+
+    @Override
+    public void load(Block block) {
+        if(padLeft < 0) padLeft = padding;
+        if(padRight < 0) padRight = padding;
+        if(padTop < 0) padTop = padding;
+        if(padBottom < 0) padBottom = padding;
+    }
+}
