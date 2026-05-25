ebd6955de8769f8db87575669252438f4f0ef4a0
The_al0ne <strashno56@gmail.com>
2026-05-26 01:46:21 +0300

    Update TerraSectorPresets.java

--- src/terra/content/TerraSectorPresets.java
+++ src/terra/content/TerraSectorPresets.java
@@ -8,78 +8,20 @@ import mindustry.world.*;
 import mindustry.content.Planets;
 
 public class TerraSectorPresets {
-    public static SectorPreset verilus;
-
-    private static class PlanetFileMapGenerator extends FileMapGenerator {
-        private final Planet planet;
-        private final SectorPreset preset;
-
-        public PlanetFileMapGenerator(Planet planet, SectorPreset preset) {
-            super(preset);
-            this.planet = planet;
-            this.preset = preset;
-        }
-
-        @Override
-        public void generate(Tiles tiles, WorldParams params) {
-            for (int x = 0; x < tiles.width; x++) {
-                for (int y = 0; y < tiles.height; y++) {
-                    if (tiles.get(x, y) == null) {
-                        tiles.set(x, y, new Tile(x, y));
-                    }
-                }
-            }
-
-            Sector sector = preset.sector;
-            if (sector == null && preset != null && preset.sector != null) {
-                sector = preset.sector;
-            }
-            if (sector == null) {
-                throw new RuntimeException("No sector available for generation");
-            }
-
-            Rules rules = Vars.state.rules;
-            Sector oldSector = rules != null ? rules.sector : null;
-            try {
-                if (rules == null) {
-                    Vars.state.rules = new Rules();
-                }
-                Vars.state.rules.sector = sector;
-
-                if (planet.generator != null) {
-                    planet.generator.generate(tiles, params);
-                } else {
-                    throw new RuntimeException("Planet has no generator");
-                }
-            } finally {
-                if (rules != null) {
-                    Vars.state.rules.sector = oldSector;
-                } else {
-                    Vars.state.rules = null;
-                }
-            }
-        }
-    }
-
-    private static class PlanetSectorPreset extends SectorPreset {
-        public PlanetSectorPreset(String name, Planet planet, int sectorId) {
-            super(name, null, Vars.mods.getMod("terra"));
-            this.generator = new PlanetFileMapGenerator(planet, this);
-            initialize(planet, sectorId);
-        }
-    }
+    public static SectorPreset 
+        verilus;
 
     public static void load(){
         //region Serpulo & Verilus
 
-        verilus = new PlanetSectorPreset("verilus", Planets.verilus, 0){{
+        /*verilus = new PlanetSectorPreset("verilus", Planets.verilus, 0){{
             alwaysUnlocked = true;
             addStartingItems = true;
             //captureWave = 180;
             difficulty = 8;
             allowLaunchSchematics = true;
             overrideLaunchDefaults = true;
-        }};
+        }};*/
         //endregion
     }
 }
