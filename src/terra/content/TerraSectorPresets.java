e0bf28d02f294d31fa5d24afb1c2659f02e117d4
The_al0ne <strashno56@gmail.com>
2026-05-25 22:59:02 +0300

    Update TerraSectorPresets.java

--- src/terra/content/TerraSectorPresets.java
+++ src/terra/content/TerraSectorPresets.java
@@ -3,17 +3,24 @@ package terra.content;
 
 import mindustry.type.*;
 import mindustry.content.Planets;
+import mindustry.Vars;
 
 import static mindustry.content.Planets.*;
 
 public class TerraSectorPresets {
     public static SectorPreset
             verilus;
-
+    private static class PlanetSectorPreset extends SectorPreset {
+        public PlanetSectorPreset(String name, Planet planet, int sector) {
+            super(name, null, Vars.mods.getMod("terra"));
+            this.generator = planet.generator;
+            initialize(planet, sector);
+        }
+    }
     public static void load(){
         //region Serpulo & Verilus
 
-        verilus = new SectorPreset("verilus", Planets.verilus, 0){{
+        verilus = new PlanetSectorPreset("verilus", Planets.verilus, 0){{
             alwaysUnlocked = true;
             addStartingItems = true;
             //captureWave = 180;
