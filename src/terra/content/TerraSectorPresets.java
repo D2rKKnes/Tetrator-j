c23e5fc13491e4e056f5ae6a66f42ae5184aca74
The_al0ne <strashno56@gmail.com>
2026-05-25 23:20:13 +0300

    Update TerraSectorPresets.java

--- src/terra/content/TerraSectorPresets.java
+++ src/terra/content/TerraSectorPresets.java
@@ -1,20 +1,49 @@
 package terra.content;
 
-
-import mindustry.type.*;
-import mindustry.content.Planets;
+import arc.struct.Seq;
 import mindustry.Vars;
+import mindustry.game.WorldParams;
+import mindustry.maps.generators.FileMapGenerator;
+import mindustry.type.*;
+import mindustry.world.Tiles;
 
 import static mindustry.content.Planets.*;
 
 public class TerraSectorPresets {
-    public static SectorPreset
-            verilus;
+    public static SectorPreset verilus;
+
+    private static class ProceduralFileMapGenerator extends FileMapGenerator {
+        private final Planet planet;
+        private final SectorPreset preset;
+
+        public ProceduralFileMapGenerator(Planet planet, SectorPreset preset) {
+            super(preset);
+            this.planet = planet;
+            this.preset = preset;
+        }
+
+        @Override
+        public void generate(Tiles tiles, WorldParams params) {
+            Sector sector = Vars.state.rules.sector;
+            if (sector == null && preset.sector != null) {
+                sector = preset.sector;
+            }
+            if (sector == null) {
+                throw new RuntimeException("No sector available for procedural generation");
+            }
+            if (planet.generator != null) {
+                planet.generator.generate(tiles, sector);
+            } else {
+                throw new RuntimeException("Planet has no generator");
+            }
+        }
+    }
+
     private static class PlanetSectorPreset extends SectorPreset {
-        public PlanetSectorPreset(String name, Planet planet, int sector) {
+        public PlanetSectorPreset(String name, Planet planet, int sectorId) {
             super(name, null, Vars.mods.getMod("terra"));
-            this.generator = planet.generator;
-            initialize(planet, sector);
+            this.generator = new ProceduralFileMapGenerator(planet, this);
+            initialize(planet, sectorId);
         }
     }
     public static void load(){
