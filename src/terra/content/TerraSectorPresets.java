71224934b0a29d1b58db2d56cfd9d8349e1e6fa8
The_al0ne <strashno56@gmail.com>
2026-05-26 23:20:00 +0300

    Update TerraSectorPresets.java

--- src/terra/content/TerraSectorPresets.java
+++ src/terra/content/TerraSectorPresets.java
@@ -1,28 +1,31 @@
 package terra.content;
 
-import mindustry.Vars;
-import mindustry.game.Rules;
-import mindustry.maps.generators.FileMapGenerator;
-import mindustry.type.*;
-import mindustry.world.*;
 import mindustry.content.Planets;
+import mindustry.mod.Mods.LoadedMod;
+import mindustry.type.*;
 
 public class TerraSectorPresets {
-    public static SectorPreset 
-        verilus;
+    public static SectorPreset verilus;
 
-    public static void load(){
-        //region Serpulo & Verilus
-
-        verilus = new SectorPreset("verilus", Planets.verilus, 0){{
+    public static void load() {
+        verilus = new NoFileSectorPreset("verilus", Planets.verilus, 0) {{
             alwaysUnlocked = true;
             addStartingItems = true;
-            //captureWave = 180;
             difficulty = 8;
-            allowLaunchSchematics = true;
             overrideLaunchDefaults = true;
-            generator = null;
         }};
-        //endregion
+    }
+
+    private static class NoFileSectorPreset extends SectorPreset {
+        public NoFileSectorPreset(String name, Planet planet, int sector) {
+            super(name, (LoadedMod) null);
+
+            this.generator = null;
+            this.planet = planet;
+            this.originalPosition = sector;
+            sector %= planet.sectors.size;
+            this.sector = planet.sectors.get(sector == -1 ? 0 : sector);
+            planet.preset(sector, this);
+        }
     }
 }
