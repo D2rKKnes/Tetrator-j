299fb044fedbb5f359a0e98bb36d68ca82d0c5dd
The_al0ne <strashno56@gmail.com>
2026-05-27 20:31:59 +0300

    shhhh

--- src/terra/content/TerraVanilaTree.java
+++ src/terra/content/TerraVanilaTree.java
@@ -55,6 +55,10 @@ public class TerraVanilaTree{
                 });
             });
         });
+        vanillaNode(erekir, shieldedWall, () -> {
+            node(shieldedWallLarge, () -> {
+            });
+        });
         vanillaNode(erekir, beamTower, () -> {
             node(beamBeacon, () -> {
             });
