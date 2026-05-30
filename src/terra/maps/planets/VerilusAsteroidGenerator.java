7a8d71570386e3126a284e3b4fab48a099ce8448
The_al0ne <strashno56@gmail.com>
2026-05-31 00:41:18 +0300

    Update VerilusAsteroidGenerator.java

--- src/terra/maps/planets/VerilusAsteroidGenerator.java
+++ src/terra/maps/planets/VerilusAsteroidGenerator.java
@@ -23,8 +23,8 @@ import mindustry.world.meta.*;
 import static mindustry.Vars.*;
 
 public class VerilusAsteroidGenerator extends BlankPlanetGenerator{
-    public int min = 26, max = 34, octaves = 1, foct = 3;
-    public float radMin = 21f, radMax = 58f, persistence = 0.28f, scale = 400f, mag = 0.91f, thresh = 1.6f;
+    public int min = 26, max = 39, octaves = 1, foct = 3;
+    public float radMin = 21f, radMax = 46f, persistence = 0.28f, scale = 400f, mag = 0.91f, thresh = 1.6f;
     public float fmag = 0.5f, fscl = 36f, fper = 0.6f;
     public float stoneChance = 0f, iceChance = 0.38f, carbonChance = 0.6f;
 
@@ -47,7 +47,7 @@ public class VerilusAsteroidGenerator extends BlankPlanetGenerator{
 
         for(int x = ax - rad; x <= ax + rad; x++){
             for(int y = ay - rad; y <= ay + rad; y++){
-                if(tiles.in(x, y) && Mathf.dst(x, y, ax, ay) / rad + Simplex.noise2d(seed, octaves, persistence, 1f / scale, x, y) * mag < thresh){
+                if(tiles.in(x, y) && Mathf.dst(x, y, ax, ay) < rad) { // / rad + Simplex.noise2d(seed, octaves, persistence, 1f / scale, x, y) * mag < thresh){
                     tiles.getn(x, y).setFloor(floor);
                 }
             }
@@ -57,7 +57,7 @@ public class VerilusAsteroidGenerator extends BlankPlanetGenerator{
     void asteroid(int ax, int ay, int rad, Floor floor) {
         for (int x = ax - rad; x <= ax + rad; x++) {
             for (int y = ay - rad; y <= ay + rad; y++) {
-                if(tiles.in(x, y) && Mathf.dst(x, y, ax, ay) / rad + Simplex.noise2d(seed, octaves, persistence, 1f / scale, x, y) * mag < thresh){
+                if(tiles.in(x, y) && Mathf.dst(x, y, ax, ay) < rad) { // / rad + Simplex.noise2d(seed, octaves, persistence, 1f / scale, x, y) * mag < thresh){
                     tiles.getn(x, y).setFloor(floor);
                 }
             }
