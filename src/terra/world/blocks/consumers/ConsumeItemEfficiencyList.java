c4ba5981ee8847050d36eb0e03808e2dc585d9c8
The_al0ne <strashno56@gmail.com>
2026-06-16 16:37:20 +0300

    Update ConsumeItemEfficiencyList.java

--- src/terra/world/blocks/consumers/ConsumeItemEfficiencyList.java
+++ src/terra/world/blocks/consumers/ConsumeItemEfficiencyList.java
@@ -8,10 +8,8 @@ public class ConsumeItemEfficiencyList extends ConsumeItemEfficiency {
     public final ObjectFloatMap<Item> multipliers;
 
     public ConsumeItemEfficiencyList(ObjectFloatMap<Item> multipliers) {
-        // Фильтр: только те предметы, которые есть в карте
         super(item -> multipliers.containsKey(item) && multipliers.get(item, 0f) > 0f);
         this.multipliers = multipliers;
-        // Для корректного отображения в статистике
         this.itemDurationMultipliers = multipliers;
     }
 
