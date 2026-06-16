440693659b76164bbea030f1d237c9109ca29c92
The_al0ne <strashno56@gmail.com>
2026-06-16 13:51:07 +0300

    Create TerraLiquids.java

---
+++ src/terra/content/TerraLiquids.java
@@ -0,0 +1,22 @@
+package terra.content;
+
+import arc.graphics.*;
+import mindustry.type.*;
+
+public class TerraLiquids{
+    public static Liquid
+  
+    carbonDioxide, fissilePlasma;
+
+    public static void load(){
+        carbonDioxide = new Liquid("carbon-dioxide", Color.valueOf("323232")){{
+            gas = true;
+        }};
+
+        fissilePlasma = new Liquid("fissile-plasma", Color.valueOf("bcff73")){{
+            gas = true;
+            temperature = 1f;
+            lightColor = Color.valueOf("bcff73").a(0.4f);
+        }};
+    }
+}
