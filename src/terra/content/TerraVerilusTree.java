3229650e1f9eb1327a0be9b4b8a95a7d15b82190
The_al0ne <strashno56@gmail.com>
2026-06-20 21:20:24 +0300

    Update TerraVerilusTree.java

--- src/terra/content/TerraVerilusTree.java
+++ src/terra/content/TerraVerilusTree.java
@@ -127,6 +127,7 @@ public class TerraVerilusTree {
                     node(parallax, () -> {
                         node(segment, () -> {});
                     });
+                    node(ejection, () -> {});
                 });
                 node(dynamics, () -> {
                     node(fracture, () -> {});
