dae6ae9f9717b5f0a0461129ae79c2dd7aab9288
The_al0ne <strashno56@gmail.com>
2026-06-24 23:30:53 +0300

    Update TerraLiquids.java

--- src/terra/content/TerraLiquids.java
+++ src/terra/content/TerraLiquids.java
@@ -11,11 +11,15 @@ public class TerraLiquids{
     public static void load(){
         carbonDioxide = new Liquid("carbon-dioxide", Color.valueOf("323232")){{
             gas = true;
+            heatCapacity = 0.8f;
+            viscosity = 0f;
+            vaporEffect = TerraFx.arcVarop;
         }};
 
         fissilePlasma = new Liquid("fissile-plasma", Color.valueOf("bcff73")){{
             gas = true;
             temperature = 3f;
+            heatCapacity = 1f;
             lightColor = Color.valueOf("bcff73").a(0.4f);
         }};
     }
