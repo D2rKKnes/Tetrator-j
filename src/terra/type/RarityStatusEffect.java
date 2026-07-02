4db280ebfc7aa0531fff01d124a219c08d3577d5
The_al0ne <strashno56@gmail.com>
2026-07-02 23:16:24 +0300

    Update RarityStatusEffect.java

--- src/terra/type/RarityStatusEffect.java
+++ src/terra/type/RarityStatusEffect.java
@@ -13,23 +13,30 @@ import mindustry.type.StatusEffect;
 
 public class RarityStatusEffect extends StatusEffect {
 
-    public Color ringColor = color;
-    public boolean drawRing = true;
+    public Color tintColor = Color.white;
+    public Float alpha = 0.4f;
+    public boolean drawAura = true;
 
     public RarityStatusEffect(String name) {
         super(name);
-        if (!Core.settings.getBool("qualityring", true)){hideDatabase = true;}
+        if (!Core.settings.getBool("unitsquality", true)){hideDatabase = true;}
         else hideDatabase = false;
     }
 
     @Override
     public void draw(Unit unit, float time) {
-        if (!Core.settings.getBool("qualityring", true) || !drawRing) return;
+        if (!Core.settings.getBool("qualityring", true) || !drawAura) return;
 
-        Draw.z(52f);
-        Draw.color(ringColor);
-        float size = unit.hitSize * 2f;
-        Draw.rect(Core.atlas.find("terra-rarity-ring"), unit.x, unit.y, size, size);
+        float unitLayer;
+        if (!unit.flying) unitLayer = unit.groundLayer;
+        else if (unit.flyingLayer != -1) unitLayer = unit.flyingLayer;
+        else if (!unit.lowAltitude) unitLayer = Layer.flyingUnit;
+        else unitLayer = Layer.flyingUnitLow;
+
+        Draw.z(unitLayer - 2);
+        Draw.color(tintColor, alpha);
+        float size = unit.hitSize * 1.15f;
+        Draw.rect(unit.previewRegion, unit.x, unit.y, size, size, unit.rotation);
         Draw.reset();
     }
 }
