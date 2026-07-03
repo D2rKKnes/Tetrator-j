7c693a19930a9ed1a6dcc18f98787e6b59bbd811
The_al0ne <strashno56@gmail.com>
2026-07-03 14:49:51 +0300

    Create AdvancedStatusEffect.java

---
+++ src/terra/type/AdvancedStatusEffect.java
@@ -0,0 +1,22 @@
+package terra.type;
+
+import arc.graphics.*;
+import arc.math.*;
+import arc.struct.*;
+import arc.util.*;
+import mindustry.*;
+import mindustry.content.*;
+import mindustry.ctype.*;
+import mindustry.type.*;
+import mindustry.entities.*;
+import mindustry.entities.units.*;
+import mindustry.gen.*;
+import mindustry.graphics.*;
+import mindustry.graphics.MultiPacker.*;
+import mindustry.world.meta.*;
+
+public class AdvancedStatusEffect extends StatusEffect{
+    public float percentDamage;
+    public float removeDamage;
+
+}
