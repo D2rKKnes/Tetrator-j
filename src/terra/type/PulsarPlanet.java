110f349a41c56372df21b61f9b473f6e7e35f85f
The_al0ne <strashno56@gmail.com>
2026-07-04 21:48:55 +0300

    Update PulsarPlanet.java

--- src/terra/type/PulsarPlanet.java
+++ src/terra/type/PulsarPlanet.java
@@ -7,9 +7,9 @@ import arc.math.geom.*;
 import arc.util.*;
 import mindustry.graphics.*;
 import mindustry.graphics.g3d.*;
+import mindustry.type.*;
 
 public class PulsarPlanet extends BetterPlanet {
-
     public float rotationSpeed = 1f;
     public Color beamColor = Color.white.cpy();
     public float beamAngleDeg = 45f;
@@ -39,44 +39,61 @@ public class PulsarPlanet extends BetterPlanet {
         Vec3 up = Tmp.v31.set(Vec3.Y);
         Vec3 dir = Tmp.v32.set(up).rotate(Vec3.X, beamAngle).rotate(Vec3.Y, azimuth).nor();
 
-        Vec3 start = Tmp.v33.set(up).scl(radius);
+        Vec3 start = Tmp.v33.set(up).scl(radius * 1.01f);
         Vec3 end = Tmp.v34.set(dir).scl(beamLength).add(start);
 
-        VertexBatch3D batch = new VertexBatch3D(beamSegments * 6);
+        VertexBatch3D batch = new VertexBatch3D(false, false, beamSegments * 6);
         batch.proj(projection);
-        batch.trans(transform);
+        batch.transform(transform);
 
-        Blending blend = Blending.additive;
-        blend.apply();
+        Blending.additive.apply();
 
         Color col = beamColor.cpy();
+        Vec3 side = Tmp.v31.set(dir).crs(up).nor();
+        if (side.len() < 0.001f) {
+            side.set(Vec3.X).crs(dir).nor();
+        }
+
+        float halfWidth = beamWidth * 0.5f;
+
+        Vec3 p1 = new Vec3();
+        Vec3 p2 = new Vec3();
+        Vec3 p1l = new Vec3();
+        Vec3 p1r = new Vec3();
+        Vec3 p2l = new Vec3();
+        Vec3 p2r = new Vec3();
 
         for (int i = 0; i < beamSegments; i++) {
-            float t1 = i / (float) beamSegments;
-            float t2 = (i + 1) / (float) beamSegments;
+            float t1 = (float) i / beamSegments;
+            float t2 = (float) (i + 1) / beamSegments;
 
             float alpha1 = 1f - t1;
             float alpha2 = 1f - t2;
 
-            Vec3 p1 = Tmp.v35.set(start).lerp(end, t1);
-            Vec3 p2 = Tmp.v36.set(start).lerp(end, t2);
+            p1.set(start).lerp(end, t1);
+            p2.set(start).lerp(end, t2);
 
-            Vec3 side = Tmp.v37.set(dir).crs(up).nor();
-            if (side.len() < 0.001f) {
-                side.set(Vec3.X).crs(dir).nor();
-            }
-            float halfWidth = beamWidth * 0.5f;
-
-            Vec3 p1l = Tmp.v38.set(p1).add(side, -halfWidth);
-            Vec3 p1r = Tmp.v39.set(p1).add(side,  halfWidth);
-            Vec3 p2l = Tmp.v40.set(p2).add(side, -halfWidth);
-            Vec3 p2r = Tmp.v41.set(p2).add(side,  halfWidth);
+            p1l.set(p1).add(side, -halfWidth);
+            p1r.set(p1).add(side,  halfWidth);
+            p2l.set(p2).add(side, -halfWidth);
+            p2r.set(p2).add(side,  halfWidth);
 
             Color c1 = col.cpy().a(alpha1);
             Color c2 = col.cpy().a(alpha2);
 
-            batch.tri(p1l, p1r, p2l, c1, c1, c2);
-            batch.tri(p1r, p2r, p2l, c1, c2, c2);
+            batch.color(c1);
+            batch.vertex(p1l);
+            batch.color(c1);
+            batch.vertex(p1r);
+            batch.color(c2);
+            batch.vertex(p2l);
+
+            batch.color(c1);
+            batch.vertex(p1r);
+            batch.color(c2);
+            batch.vertex(p2r);
+            batch.color(c2);
+            batch.vertex(p2l);
         }
 
         batch.flush(Gl.triangles);
