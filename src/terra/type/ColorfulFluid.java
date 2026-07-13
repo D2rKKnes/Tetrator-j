0c8eb31c2452f1feb0b56484a93ff77c41d6913c
The_al0ne <strashno56@gmail.com>
2026-07-13 21:03:50 +0300

    Update ColorfulFluid.java

--- src/terra/type/ColorfulFluid.java
+++ src/terra/type/ColorfulFluid.java
@@ -8,21 +8,22 @@ import arc.math.Mathf;
 import mindustry.game.EventType;
 import mindustry.type.Liquid;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ColorfulFluid extends Liquid {
     private static final List<ColorfulFluid> instances = new ArrayList<>();
 
-    private final Color[] colors;
+    private final Color[] colorPalette;
 
     private Color originalColor;
     private TextureRegion originalFullIcon;
     private TextureRegion originalUiIcon;
 
-    public ColorfulFluid(String name, Color baseColor) {
+    public ColorfulFluid(String name, Color baseColor, Color... colors) {
         super(name, baseColor);
         this.originalColor = baseColor.cpy();
-        this.colors = (colorArray != null && colorArray.length > 0) ? colorArray : new Color[0];
+        this.colorPalette = (colors != null && colors.length > 0) ? colors : new Color[0];
         instances.add(this);
     }
 
@@ -39,13 +40,13 @@ public class ColorfulFluid extends Liquid {
         TextureRegion foundIcon = null;
         Color foundColor = null;
 
-        if (neonEnabled && colors.length > 0) {
-            index = Mathf.random(colors.length - 1);
+        if (neonEnabled && colorPalette.length > 0) {
+            index = Mathf.random(colorPalette.length - 1);
             String spriteName = this.name + "-" + index;
             TextureRegion region = Core.atlas.find(spriteName);
             if (region.found()) {
                 foundIcon = region;
-                foundColor = colors[index];
+                foundColor = colorPalette[index];
             } else {
                 index = -1;
             }
