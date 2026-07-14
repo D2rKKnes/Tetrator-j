6f417c0b3936350056b9614222091596abcfbebb
The_al0ne <strashno56@gmail.com>
2026-07-14 21:29:30 +0300

    Update TerraVanilaTree.java

--- src/terra/content/TerraVanilaTree.java
+++ src/terra/content/TerraVanilaTree.java
@@ -77,6 +77,9 @@ public class TerraVanilaTree{
                 });
             });
         });
+        vanillaNode(payloadConveyor, () -> {
+            node(largePayloadConveyor, () -> {});
+        });
         // Erekir
         String erekir = "erekir";
         vanillaNode(erekir, berylliumWallLarge, () -> {
