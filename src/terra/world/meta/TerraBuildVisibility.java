07e05c9a01d246048820ab05e787b5d778c20174
The_al0ne <strashno56@gmail.com>
2026-09-13 18:42:58 +0300

    Update TerraBuildVisibility.java

--- src/terra/world/meta/TerraBuildVisibility.java
+++ src/terra/world/meta/TerraBuildVisibility.java
@@ -6,5 +6,5 @@ import mindustry.content.*;
 import mindustry.world.meta.*;
 
 public class TerraBuildVisibility{
-    specialContent = new BuildVisibility(() -> Core.settings.getBool("enableblocks", false) ? true : (Vars.state == null || Vars.state.rules.infiniteResources));
+    specialContent = new BuildVisibility(() -> Core.settings.getBool("enableblocks", false) || Vars.state == null || Vars.state.rules.infiniteResources));
 }
