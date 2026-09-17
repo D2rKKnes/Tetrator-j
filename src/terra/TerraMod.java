31104b18c0237039433716fee01abf6a9c25ba5d
The_al0ne <strashno56@gmail.com>
2026-09-17 15:42:42 +0300

    Update TerraMod.java

--- src/terra/TerraMod.java
+++ src/terra/TerraMod.java
@@ -57,7 +57,7 @@ public class TerraMod extends Mod{
         TerraBlocks.load();
         TerraSectorPresets.load();
         TerraPlanets.load();
-        TerraVanillaBlocksOverride.load();
+        TerraVanillaOverride.load();
         TerraVanilaTree.load();
         TerraVerilusTree.load();
     }
