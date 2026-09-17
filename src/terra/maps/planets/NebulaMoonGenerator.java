7d5d1d8b54b61ec5494eed674645d79c62ee3295
The_al0ne <strashno56@gmail.com>
2026-09-17 15:36:35 +0300

    Update NebulaMoonGenerator.java

--- src/terra/maps/planets/NebulaMoonGenerator.java
+++ src/terra/maps/planets/NebulaMoonGenerator.java
@@ -19,7 +19,7 @@ import mindustry.world.blocks.environment.*;
 import terra.graphics.g3d.*;
 
 public class NebulaMoonGenerator extends PlanetGenerator {
-    public double octaves = 4, persistence = 0.8, scl = 1.6, pow = 1.7, mag = 2;
+    public double octaves = 4, persistence = 0.8, scl = 0.4, pow = 1.7, mag = 2;
     float heightYOffset = 41.3f;
     float sclh = 5f;
     float waterOffset = 0.04f;
