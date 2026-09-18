d3fed0db5966de8957d11924d847ac80c02f0ee0
oneonel <asqjreqiejr@gmail.com>
2026-09-18 16:01:47 +0300

    Refactor RouterSorter for better item management
    Refactor RouterSorter class to update package and improve item handling logic.

--- src/terra/world/blocks/RouterSorter.java
+++ src/terra/world/blocks/RouterSorter.java
@@ -1,31 +1,23 @@
-package terra.world.blocks;
+package example;
 
+import arc.Core;
+import arc.graphics.Color;
+import arc.graphics.g2d.Draw;
 import arc.scene.ui.layout.Table;
 import arc.util.io.Reads;
 import arc.util.io.Writes;
-import mindustry.content.Items;
 import mindustry.gen.Building;
 import mindustry.gen.Icon;
-import mindustry.type.Category;
 import mindustry.type.Item;
-import mindustry.type.ItemStack;
 import mindustry.world.blocks.distribution.Sorter;
 
 public class RouterSorter extends Sorter {
 
-    // public static RouterSorter routerSorter;
-
-    // public static void setup() {
-    //     routerSorter = new RouterSorter("router-sorter");
-    // }
-
     public RouterSorter(String name) {
         super(name);
         configurable = true;
-
-        // requirements(Category.distribution, ItemStack.with(Items.lead, 3, Items.graphite, 2));
-        // buildCostMultiplier = 4f;
-        // health = 85;
+        drawDynamic = true;
+        drawCached = false;
     }
 
     public class RouterSorterBuild extends SorterBuild {
@@ -45,64 +37,67 @@ public class RouterSorter extends Sorter {
         }
 
         @Override
-        public boolean acceptItem(Building source, Item item) {
-            if (sortItem == null) {
-                return team == source.team;
-            }
-            return super.acceptItem(source, item);
-        }
+        public Building getTileTarget(Item item, Building source, boolean flip) {
+            int dir = source.relativeTo(tile.x, tile.y);
+            if (dir == -1) return null;
 
-        @Override
-        public Building getTileTarget(Item item, Building source, boolean inv) {
             if (sortItem == null) {
                 int len = proximity.size;
                 if (len == 0) return null;
 
                 for (int i = 0; i < len; i++) {
                     Building other = proximity.get((cdump + i) % len);
-                    if (other != source && other.acceptItem(this, item)) {
-                        cdump = (cdump + i + 1) % len;
+                    if (other != source && other.acceptItem(this, item) && other.team == team) {
+                        if (flip) {
+                            cdump = (cdump + i + 1) % len;
+                        }
                         return other;
                     }
                 }
                 return null;
             }
-            int dir = relativeTo(source);
-            if (dir == -1) return null;
 
-            boolean matches = (item == sortItem);
-            boolean passStraight = invert ? !matches : matches;
-
-            if (passStraight) {
-                return nearby(dir ^ 2);
+            if (((item == sortItem) != invert) == enabled) {
+                if (isSame(source) && isSame(nearby(dir))) {
+                    return null;
+                }
+                return nearby(dir);
             } else {
-                Building left = nearby((dir + 1) % 4);
-                Building right = nearby((dir + 3) % 4);
-                boolean leftValid = left != null && left.acceptItem(this, item);
-                boolean rightValid = right != null && right.acceptItem(this, item);
+                Building a = nearby((dir + 3) % 4);
+                Building b = nearby((dir + 1) % 4);
+                boolean ac = a != null && !(a.block.instantTransfer && source.block.instantTransfer) && a.acceptItem(this, item);
+                boolean bc = b != null && !(b.block.instantTransfer && source.block.instantTransfer) && b.acceptItem(this, item);
 
-                if (leftValid && rightValid) {
-                    cdump = (cdump + 1) % 2;
-                    return cdump == 0 ? left : right;
-                } else if (leftValid) {
-                    return left;
-                } else if (rightValid) {
-                    return right;
+                if (ac && !bc) {
+                    return a;
+                } else if (bc && !ac) {
+                    return b;
+                } else if (!bc) {
+                    return null;
+                } else {
+                    Building to = (rotation & (1 << dir)) == 0 ? a : b;
+                    if (flip) rotation ^= (1 << dir);
+                    return to;
                 }
             }
-
-            return null;
         }
 
         @Override
-        public void handleItem(Building source, Item item) {
-            if (sortItem == null) {
-                Building target = getTileTarget(item, source, false);
-                if (target != null) {
-                    target.handleItem(this, item);
-                }
-            } else {
-                super.handleItem(source, item);
+        public void draw() {
+            var router = Core.atlas.find(name);
+            Draw.rect(router, x, y);
+
+            if (sortItem != null) {
+                var top = Core.atlas.find(name + "-top");
+                Draw.color(sortItem.color);
+                Draw.rect(top, x, y);
+                Draw.color();
+            }
+
+            if (invert && sortItem != null) {
+                var invertTex = Core.atlas.find(name + "-invert");
+                Draw.color();
+                Draw.rect(invertTex, x, y);
             }
         }
 
