0e9a062dc4a4ad4acfdc6b7a0a15b59a28a5a0e0
The_al0ne <strashno56@gmail.com>
2026-09-18 14:26:09 +0300

    Create TerraLoadouts.java

---
+++ src/terra/content/TerraLoadouts.java
@@ -0,0 +1,12 @@
+package terra.content;
+
+import mindustry.game.*;
+
+public class Loadouts{
+    public static Schematic
+    basicRay;
+
+    public static void load(){
+        basicRay = Schematics.readBase64("bXNjaAF4nBWJwQqAIBAFXykdCvoTP6UviA6b7UEwlXUvEf17CsMcZmBgLGafk3LSjQrG98MU6eRYMe4HbKKbYYQeLBdXL6FoyAkYsCqLkPNZ2PXfW6PpB9vLFX0=");
+    }
+}
