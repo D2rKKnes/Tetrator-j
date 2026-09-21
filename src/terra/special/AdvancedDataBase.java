fc2a78d1e4ecce31f26533161474b0172a5c67b6
The_al0ne <strashno56@gmail.com>
2026-09-21 17:06:54 +0300

    Update AdvancedDataBase.java

--- src/terra/special/AdvancedDataBase.java
+++ src/terra/special/AdvancedDataBase.java
@@ -22,11 +22,11 @@ import terra.type.*;
 import static mindustry.Vars.*;
 
 public class AdvancedDataBase {
-    static final Stat
-        twstatus = new Stat("terraweatherstatus", StatCat.function),
-        twwind = new Stat("terraweatherwind", StatCat.function),
-        twliquid = new Stat("terraweatherliquid", StatCat.function),
-        twattrs = new Stat("terraweatherattrs", StatCat.function);
+    // static final Stat
+    //     twstatus = new Stat("terraweatherstatus", StatCat.function),
+    //     twwind = new Stat("terraweatherwind", StatCat.function),
+    //     twliquid = new Stat("terraweatherliquid", StatCat.function),
+    //     twattrs = new Stat("terraweatherattrs", StatCat.function);
 
     public static final Seq<WeatherEntry> entries = new Seq<>();
 
