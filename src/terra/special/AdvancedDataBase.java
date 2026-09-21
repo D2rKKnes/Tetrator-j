f35c015d203d190605eac63011beeede51648bfc
The_al0ne <strashno56@gmail.com>
2026-09-21 19:52:55 +0300

    Update AdvancedDataBase.java

--- src/terra/special/AdvancedDataBase.java
+++ src/terra/special/AdvancedDataBase.java
@@ -37,12 +37,12 @@ public class AdvancedDataBase {
             entries.add(entry);
 
             if (w.fullIcon == null || !w.fullIcon.found()) {
-                entry.fullIcon = Icon.rainSmall.getRegion();
+                entry.fullIcon = Icon.rain.getRegion();
             } else {
                 entry.fullIcon = w.fullIcon;
             }
             if (w.uiIcon == null || !w.uiIcon.found()) {
-                entry.uiIcon = Icon.rainSmall.getRegion();
+                entry.uiIcon = Icon.rain.getRegion();
             } else {
                 entry.uiIcon = w.uiIcon;
             }
