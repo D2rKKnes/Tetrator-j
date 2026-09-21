8eade724dc3e21a34f1008eda26d36529c77229f
The_al0ne <strashno56@gmail.com>
2026-09-21 17:01:07 +0300

    Update WeatherEntry.java

--- src/terra/type/WeatherEntry.java
+++ src/terra/type/WeatherEntry.java
@@ -4,10 +4,12 @@ import arc.*;
 import arc.scene.ui.layout.*;
 import mindustry.ctype.*;
 import mindustry.game.*;
+import terra.special.*;
 
 /** This class is only for displaying weather in the content database. */
 public class WeatherEntry extends UnlockableContent{
     private static final String bundleContentPrefix = "weather";
+    public Weather weather = Weathers.rain;
 
     public WeatherEntry(String name){
         super(name);
@@ -17,9 +19,28 @@ public class WeatherEntry extends UnlockableContent{
         databaseCategory = "weather";
     }
 
-    public WeatherEntry(String name, boolean alwaysUnlocked){
+    public WeatherEntry(String name, Weather weather){
         this(name);
-        this.alwaysUnlocked = alwaysUnlocked;
+        this.weather = weather;
+    }
+
+    @Override
+    public void setStats(){
+        if (weather.status != StatusEffects.none) {
+            stats.add(AdvancedDataBase.twstatus, weather.status.emoji() + weather.status.localizedName);
+            stats.add(Stat.targetsAir,   weather.statusAir);
+            stats.add(Stat.targetsGround, weather.statusGround);
+        }
+
+        if (weather instanceof ParticleWeather pw && pw.force > 0) {
+            stats.add(AdvancedDataBase.twwind, pw.force, StatUnit.tilesSecond);
+        }
+
+        if (weather instanceof RainWeather rw && rw.liquid != null) {
+            stats.add(AdvancedDataBase.twliquid, rw.liquid.emoji() + rw.liquid.localizedName);
+        }
+
+        AdvancedDataBase.attri(this.stats, weather.attrs);
     }
 
     // @Override
