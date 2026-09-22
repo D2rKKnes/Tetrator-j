194f43c6e9187159457d584d8ff0c68e26060763
The_al0ne <strashno56@gmail.com>
2026-09-22 12:21:17 +0300

    Update LoreEntry.java

--- src/terra/type/LoreEntry.java
+++ src/terra/type/LoreEntry.java
@@ -31,8 +31,8 @@ public class LoreEntry extends UnlockableContent{
     @Override
     public void loadIcon(){
         super.loadIcon();
-        if(fullIcon == null || !fullIcon.found()) fullIcon = Core.atlas.find("terra-book");
-        if(uiIcon == null || !uiIcon.found()) uiIcon = Core.atlas.find("terra-book");
+        if(fullIcon == null || !fullIcon.found()) fullIcon = Icon.book.getRegion();
+        if(uiIcon == null || !uiIcon.found()) uiIcon = Icon.book.getRegion();
     }
 
     @Override
